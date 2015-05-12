b9347390c2db09f21af107a3b0d1f952d49b84b9
Junior Andrade <seisxis@gmail.com>
2015-05-12 16:06:38 -0300

    Update enable_trigger_multiple_tables.sql

--- enable_trigger_multiple_tables.sql
+++ enable_trigger_multiple_tables.sql
@@ -1,7 +1,13 @@
--- enable audit trigger to all tables of a specific schema
-
+-- CREATE TRIGGERS
 select 'CREATE TRIGGER audit_trigger_stm AFTER TRUNCATE ON '||table_name|| ' FOR EACH STATEMENT EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
 
 select 'CREATE TRIGGER audit_trigger_stm_row AFTER INSERT OR DELETE OR UPDATE ON '||table_name|| ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
+from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
+
+-- ENABLE TRIGGERS
+select 'ALTER TABLE '||table_name||' ENABLE TRIGGER audit_trigger_stm;'
+from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
+
+select 'ALTER TABLE '||table_name||' ENABLE TRIGGER audit_trigger_stm_row;'
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';

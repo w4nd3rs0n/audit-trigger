c21ccc5da67c6d92f0f6144df04ca25ec9e48a00
Junior Andrade <seisxis@gmail.com>
2015-05-15 10:12:29 -0300

    Update enable_trigger_multiple_tables.sql

--- enable_trigger_multiple_tables.sql
+++ enable_trigger_multiple_tables.sql
@@ -1,8 +1,8 @@
 -- CREATE TRIGGERS
-select 'CREATE TRIGGER audit_trigger_stm AFTER TRUNCATE ON '||table_name|| ' FOR EACH STATEMENT EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
+select 'DROP TRIGGER IF EXISTS audit_trigger_stm ON '||table_name|| '; CREATE TRIGGER audit_trigger_stm AFTER TRUNCATE ON '||table_name|| ' FOR EACH STATEMENT EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
 
-select 'CREATE TRIGGER audit_trigger_stm_row AFTER INSERT OR DELETE OR UPDATE ON '||table_name|| ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
+select 'DROP TRIGGER IF EXISTS audit_trigger_stm ON '||table_name|| '; CREATE TRIGGER audit_trigger_stm_row AFTER INSERT OR DELETE OR UPDATE ON '||table_name|| ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(''true'');' 
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
 
 -- ENABLE TRIGGERS

efedeca90d0bd36ebe4e85311889e682162e7492
Junior Andrade <seisxis@gmail.com>
2015-05-15 10:19:23 -0300

    Update enable_trigger_multiple_tables.sql

--- enable_trigger_multiple_tables.sql
+++ enable_trigger_multiple_tables.sql
@@ -1,10 +1,12 @@
 ------------------------------------------------------------------------
 -- CREATE TRIGGERS
 ------------------------------------------------------------------------
+-- TRUNCATE
 select 'DROP TRIGGER IF EXISTS audit_trigger_stm ON '||table_name|| '; CREATE TRIGGER audit_trigger_stm AFTER TRUNCATE ON '||table_name|| ' FOR EACH STATEMENT EXECUTE PROCEDURE audit.if_modified_func(''true''); ALTER TABLE '||table_name||' ENABLE TRIGGER audit_trigger_stm;' 
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
 
-select 'DROP TRIGGER IF EXISTS audit_trigger_stm ON '||table_name|| '; CREATE TRIGGER audit_trigger_stm_row AFTER INSERT OR DELETE OR UPDATE ON '||table_name|| ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(''true''); ALTER TABLE '||table_name||' ENABLE TRIGGER audit_trigger_stm_row;' 
+-- INSERT/UPDATE/DELETE
+select 'DROP TRIGGER IF EXISTS audit_trigger_stm_row ON '||table_name|| '; CREATE TRIGGER audit_trigger_stm_row AFTER INSERT OR DELETE OR UPDATE ON '||table_name|| ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(''true''); ALTER TABLE '||table_name||' ENABLE TRIGGER audit_trigger_stm_row;' 
 from information_schema.tables where table_schema = 'public' and table_name ilike 'tb_%';
 
 ------------------------------------------------------------------------
